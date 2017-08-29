429fcd4351bae54c4186abfce48b908ed9f12c16
Naz Khan <nazmuskhan@yahoo.com>
2017-08-28 22:59:33 -0400

    moar changes

--- nelp-views/src/components/info_card.js
+++ nelp-views/src/components/info_card.js
@@ -10,12 +10,13 @@ export default function InfoCard(props) {
         <img src={props.properties.image_url} height='200' width='300'/>
       </div>
       <div className="info-details">
-        <h3>
-          Rating: {props.properties.rating}
-        </h3>
-        <h3>
-          Price: {props.properties.price}
-        </h3>
+        <h4> Rating: {props.properties.rating} </h4>
+        <h4> Price: {props.properties.price} </h4>
+        <h4> Phone: {props.properties.display_phone} </h4>
+        <h5> {props.properties.location.address1}</h5>
+        <h5> {props.properties.location.city}</h5>
+        <h5> {props.properties.location.zip_code}</h5>
+        <a className='link-button' href={props.properties.url} target='_blank'>Website</a>
       </div>
     </div>
   )
